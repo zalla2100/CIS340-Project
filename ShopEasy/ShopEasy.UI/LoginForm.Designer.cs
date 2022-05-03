7cc0dd45e19395b822c36ab8a52c33ca7a26d341
zalla2100 <tcorazalla@gmail.com>
2022-05-03 16:15:56 -0500

    initial login form design

---
+++ ShopEasy/ShopEasy.UI/LoginForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace ShopEasy.UI
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shopEasyLbl = new System.Windows.Forms.Label();
+            this.usernameLbl = new System.Windows.Forms.Label();
+            this.usernameTxtBx = new System.Windows.Forms.TextBox();
+            this.passwordLbl = new System.Windows.Forms.Label();
+            this.passwordTxtBx = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // shopEasyLbl
+            // 
+            this.shopEasyLbl.AutoSize = true;
+            this.shopEasyLbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.shopEasyLbl.Location = new System.Drawing.Point(324, 9);
+            this.shopEasyLbl.Name = "shopEasyLbl";
+            this.shopEasyLbl.Size = new System.Drawing.Size(145, 41);
+            this.shopEasyLbl.TabIndex = 0;
+            this.shopEasyLbl.Text = "ShopEasy";
+            // 
+            // usernameLbl
+            // 
+            this.usernameLbl.AutoSize = true;
+            this.usernameLbl.Location = new System.Drawing.Point(298, 118);
+            this.usernameLbl.Name = "usernameLbl";
+            this.usernameLbl.Size = new System.Drawing.Size(75, 20);
+            this.usernameLbl.TabIndex = 1;
+            this.usernameLbl.Text = "Username";
+            // 
+            // usernameTxtBx
+            // 
+            this.usernameTxtBx.Location = new System.Drawing.Point(298, 158);
+            this.usernameTxtBx.Name = "usernameTxtBx";
+            this.usernameTxtBx.Size = new System.Drawing.Size(198, 27);
+            this.usernameTxtBx.TabIndex = 2;
+            // 
+            // passwordLbl
+            // 
+            this.passwordLbl.AutoSize = true;
+            this.passwordLbl.Location = new System.Drawing.Point(297, 213);
+            this.passwordLbl.Name = "passwordLbl";
+            this.passwordLbl.Size = new System.Drawing.Size(70, 20);
+            this.passwordLbl.TabIndex = 3;
+            this.passwordLbl.Text = "Password";
+            // 
+            // passwordTxtBx
+            // 
+            this.passwordTxtBx.Location = new System.Drawing.Point(298, 251);
+            this.passwordTxtBx.Name = "passwordTxtBx";
+            this.passwordTxtBx.PasswordChar = '*';
+            this.passwordTxtBx.Size = new System.Drawing.Size(198, 27);
+            this.passwordTxtBx.TabIndex = 4;
+            this.passwordTxtBx.UseSystemPasswordChar = true;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(344, 349);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(94, 29);
+            this.loginBtn.TabIndex = 5;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordTxtBx);
+            this.Controls.Add(this.passwordLbl);
+            this.Controls.Add(this.usernameTxtBx);
+            this.Controls.Add(this.usernameLbl);
+            this.Controls.Add(this.shopEasyLbl);
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label shopEasyLbl;
+        private System.Windows.Forms.Label usernameLbl;
+        private System.Windows.Forms.TextBox usernameTxtBx;
+        private System.Windows.Forms.Label passwordLbl;
+        private System.Windows.Forms.TextBox passwordTxtBx;
+        private System.Windows.Forms.Button loginBtn;
+    }
+}
+
