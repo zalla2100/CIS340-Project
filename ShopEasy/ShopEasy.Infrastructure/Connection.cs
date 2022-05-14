aadcf69aaae24ac9929d0d193c47be7a4c9f635f
zalla2100 <tcorazalla@gmail.com>
2022-05-14 18:46:16 -0500

    removed context scaffold comment

--- ShopEasy/ShopEasy.Infrastructure/Connection.cs
+++ ShopEasy/ShopEasy.Infrastructure/Connection.cs
@@ -7,9 +7,5 @@ namespace ShopEasy.Infrastructure
     {
         public static string ConnectionString =>
             ConfigurationManager.ConnectionStrings["ShopEasyDB"].ConnectionString;
-
-        /*
-          Scaffold-DbContext –Connection "Data Source=(LocalDB)\MSSQLLocalDB;AttachDBFilename='|DataDirectory|\ShopEasyDB.mdf'; Integrated Security=True" -Provider  Microsoft.EntityFrameworkCore.SqlServer  -OutputDir Models\DataLayer -Context ShopEasyDBContext -DataAnnotations –Force
-         */
     }
 }
